0c85932b82754ec496f4d7cbc9ebc6aa98651ce8
Bogdan Buduroiu <bbuduroiu@gmail.com>
2016-05-08 13:57:54 +0100

    Added filtering by keyword and Category
    Still buggy, as refreshing resets category & search

--- model/src/org/bogdanbuduroiu/auction/model/comms/message/DataRequest.java
+++ model/src/org/bogdanbuduroiu/auction/model/comms/message/DataRequest.java
@@ -1,5 +1,6 @@
 package org.bogdanbuduroiu.auction.model.comms.message;
 
+import org.bogdanbuduroiu.auction.model.Category;
 import org.bogdanbuduroiu.auction.model.User;
 
 /**
@@ -8,18 +9,43 @@ import org.bogdanbuduroiu.auction.model.User;
 public class DataRequest extends Message {
 
     private final DataRequestType DATA_REQUEST_TYPE;
-    private User user;
+    private final User USER;
+    private final Category CATEGORY;
+    private final String KEYWORD;
 
     public DataRequest(User sender, DataRequestType DATA_REQUEST_TYPE) {
+        this(sender, DATA_REQUEST_TYPE, Category.ALL);
+    }
+
+    public DataRequest(User sender, DataRequestType DATA_REQUEST_TYPE, Category category) {
         super(sender, MessageType.DATA_REQUEST);
         this.DATA_REQUEST_TYPE = DATA_REQUEST_TYPE;
+        this.CATEGORY = category;
+        this.USER = sender;
+        this.KEYWORD = "";
+    }
+
+    public DataRequest(User sender, DataRequestType DATA_REQUEST_TYPE, String keyword) {
+        super(sender, MessageType.DATA_REQUEST);
+        this.DATA_REQUEST_TYPE = DATA_REQUEST_TYPE;
+        this.CATEGORY = Category.ALL;
+        this.USER = sender;
+        this.KEYWORD = keyword;
     }
 
     public DataRequestType data_req_type() {
         return DATA_REQUEST_TYPE;
     }
 
+    public Category getCategory() {
+        return CATEGORY;
+    }
+
+    public String getKeyword() {
+        return KEYWORD;
+    }
+
     public User getUser() {
-        return this.user;
+        return this.USER;
     }
 }
